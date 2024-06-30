5bb19130d9f61dfe58477136fab1f57122f57b7b
norabc22 <norabc_22@hotmail.com>
2024-06-30 19:17:44 -0300

    commit actualizacion Nora
    Subiendo archivos  actualizados

--- index.js
+++ index.js
@@ -2,21 +2,28 @@
 let port = 3000;
 const express = require('express');
 const app = express();
-const path = require('path');
-const usuariosRouter = require('./routes/usuarios');
 
+const path = require('path');
+
+const productosRouter = require('./routes/productos');
 app.use(express.json());
-app.use('/usuarios',usuariosRouter);
-app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/productos',productosRouter);
+//app.use(express.static(path.join(__dirname,'public')));
 
 
 
 //AGREGADO DE MULTER
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.get('/', (req,res) => 
+    {
+        res.send('HOLA DESDE EL PUERTO LOCALHOST:3000');
+    });
+    
 
 app.listen(port , () => 
 {
-    console.log(`Servidor ejecutandose en el puert ${port}`)
+    console.log(`Servidor ejecutandose en el puerto ${port}`)
 });
 
