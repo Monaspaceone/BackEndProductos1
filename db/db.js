f83ffcad8f61d1b273e6938f02838bc75e048a91
SoledadOsk <112733823+Monaspaceone@users.noreply.github.com>
2024-07-10 01:40:35 -0300

    Update db.js
    puse unicamente la conexion, saque la base creada localmente

--- db/db.js
+++ db/db.js
@@ -1,14 +1,45 @@
 require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Error conectando a la base de datos:", err);
+        return;
+    }
+    
+    console.log("Conectado a la base de datos");
+
+    // Puedes ejecutar otras inicializaciones aquí si es necesario
+});
+
+module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*require('dotenv').config();
 const mySql = require('mysql2'); 
 
  const connection = mySql.createConnection(
-  /*  {
-        host : 'localhost',
-        user: 'root',
-        //password: 'norabc22mysql',
-        database: 'tienda_ropa'
-    });
-    */
+
 
  {
  host: process.env.DB_HOST,
@@ -111,11 +142,11 @@ connection.connect((err) =>
 
 //EXPORTO EL MODULO CONNECTION PARA SER USADO EN OTRO MODULO
 
-module.exports = connection;
+//module.exports = connection;
 
 
 
- /*   connection.connect((err) => 
+ /*  connection.connect((err) => 
     {
         if(err)
         {
