4f50721e763d850a3fdd8e5c4befc9b781debe5e
SoledadOsk <soledadinmb@gmail.com>
2024-07-22 21:08:58 -0300

    prueba
    ...

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /*--------SERVIDOR ESTATICO CON EXPRESS-------*/
 let port = 3000;
+
 require('dotenv').config(); //AGREGUE ESTO
 const express = require('express');
 const app = express();
 
 const path = require('path'); //une el dirname con la carpeta public
+
+//archivo en ...
+
 
 const productosRouter = require('./routes/productos');
 
